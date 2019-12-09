574eab07b9c3aac14e55eda3175e4a5c507c9656
Heasley <heasleys4hemp@hotmail.com>
2019-12-09 13:10:45 -0500

    Filter selectors

--- playerfilter.user.js
+++ playerfilter.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         PlayerFilter
 // @namespace    Heasleys.PlayerFilter
-// @version      1.2
-// @description  Filter players from advanced user search (version: find players not in a faction and also have donator/subscriber status)
+// @version      2.0
+// @description  Filter players from advanced user search
 // @author       Heasleys4hemp [1468764]
 // @include     *.torn.com/userlist.php*
 // @grant        GM_addStyle
@@ -10,98 +10,200 @@
 // ==/UserScript==
 
 GM_addStyle(`
-.pf_container {
+.wb_container {
   display: flex;
   flex-direction: column;
 }
-.pf_title {
-  background-color: #CCCC00;
+.wb_title {
+  border: 1px solid rgba(0, 0, 0, .5);
+  border-bottom: none;
   border-radius: 5px 5px 0px 0px;
-  color: #FFFFFF;
-  font-size: 1.5em;
+  color: #ffffff;
+  font-size: 13px;
+  letter-spacing: 1px;
+  text-shadow: rgba(0, 0, 0, 0.65) 1px 1px 2px;
+  font-weight: 400;
+  padding-left: 10px;
+  margin-top: 10px;
+  background-color: rgb(202, 185, 0);
+  background-image: linear-gradient(90deg, transparent 50%, rgba(0, 0, 0, 0.07) 0px);
+  background-size: 4px;
+  height: 30px;
+  line-height: 30px;
 }
-.pf_content {
+.wb_content {
   background-color: #F2F2F2;
+  border: 1px solid rgba(0, 0, 0, .5);
+  border-top: none;
 }
 
-.pf_row {
+.wb_row {
   display: flex;
-  margin: 0.5em;
+  margin: 0.75em;
 }
 
-.pf_row p {
-  margin: 0.5em;
+.wb_col {
+  margin-left: 20px;
 }
-`)
+
+.wb_col > p {
+  font-weight: bold;
+  font-size: 16px;
+  border-bottom: 1px solid #363636;
+  margin-bottom: 3px;
+}
+
+.wb_col input {
+  vertical-align: middle;
+}
+
+.checkboxes span {
+  vertical-align: middle;
+}
+
+.wb_col ul>li {
+  display: block;
+  padding-right: 10px;
+  padding-left: 22px;
+  text-indent: -22px;
+  padding-bottom: 3px;
+}
+`);
+
+$(window).load(function () {
 
 
-const insert_xgr_html = new Promise((resolve, reject) => {
-  unsafeWindow.addEventListener('load', () => {
     document.querySelector('.content-wrapper').insertAdjacentHTML('afterbegin', `
-    <div class="pf_container">
-      <div class="pf_title m-top10 title-brown top-round">Filter Toggles</div>
-       <div class="pf_content">
-        <div class="pf_row">
-          <div class="multiSelect">
-            <ul>
+    <div class="wb_container">
+      <div class="wb_title">Filter Toggles</div>
+       <div class="wb_content">
+        <div class="wb_row">
+          <div class="wb_col">
+            <p>Hide</p>
+            <ul class="filter">
                 <li>
-                    <input type="checkbox" value="#icon9" />In a Faction</li>
+                    <input type="checkbox" value="#icon9" /><span> In a Faction</span></li>
                 <li>
-                    <input type="checkbox" value="#icon74" />Faction Leader</li>
+                    <input type="checkbox" value="#icon74" /><span> Faction Leader</span></li>
                 <li>
-                    <input type="checkbox" value="#icon70" />Federal Jail</li>
+                    <input type="checkbox" value="#icon70" checked/><span> Federal Jail</span></li>
                 <li>
-                    <input type="checkbox" value="#icon72" />New Leaf</li>
+                    <input type="checkbox" value="#icon72" /><span> New Leaf</span></li>
                 <li>
-                    <input type="checkbox" value="#icon27" />In a Company</li>
+                    <input type="checkbox" value="#icon27" /><span> In a Company</span></li>
                 <li>
-                    <input type="checkbox" value="#icon8" />Married</li>
+                    <input type="checkbox" value="#icon73" /><span> Company Director</span></li>
             </ul>
-        </div>
+          </div>
+
+          <div class="wb_col">
+          <p>Has</p>
+            <ul class="have">
+                <li>
+                    <input type="checkbox" value="li#icon3" checked/><span>Donator</span></li>
+                <li>
+                    <input type="checkbox" value="li#icon4" checked/><span>Subscriber</span></li>
+                <li>
+                    <input type="checkbox" value="li#icon70"/><span>Federal Jail</span></li>
+                <li>
+                    <input type="checkbox" value="li#icon77"/><span>Resting in Peace</span></li>
+            </ul>
+          </div>
+
+
         </div>
       </div>
     </div>
     <hr class="delimiter-999 m-top10">
-    `)
-    resolve(true)
-  })
-})
+    `);
 
-//icon3 = donator, icon4 = subscriber
-//icon6 = male, icon7 = female, icon8 = married, icon9 = faction, icon27 = company
-//icon15 = hospital, icon16 = jail, icon70 = federal, icon71 = traveling, icon72 = new leaf, icon74 = leader of faction
 
-var arrList = ['#icon9', '#icon70', '#icon74']; //filter out faction, faction leader, and fed
+//icon3 = donator
+//icon4 = subscriber
+//icon6 = male
+//icon7 = female
+//icon8 = married
+//icon9 = faction
+//icon15 = hospital
+//icon16 = jail
+//icon27 = company
+//icon70 = federal
+//icon71 = traveling
+//icon72 = new leaf
+//icon73 =company director
+//icon74 = faction leader
 
-//var arrList = ['#icon6', '#icon7', '#icon15', '#icon16', '#icon71']; //filter out everything except fed
 
-const observer = new MutationObserver(() => {
+
+function filter() {
+
+    var arrList = [];
+    $('ul.filter > li > input[type=checkbox]').each(function () {
+        if($(this).is(":checked")){
+                var checkval = $(this).val();
+                arrList.push(checkval);
+            }
+    });
+
+    var haveList = [];
+    $('ul.have > li > input[type=checkbox]').each(function () {
+        if($(this).is(":checked")){
+                var checkval = $(this).val();
+                haveList.push(checkval);
+            }
+    });
+
+    var haveListvar = "";
+    if (haveList.length < 1) {
+        haveListvar = "li";
+    } else {
+    haveList.forEach(element => haveListvar += element + ",");
+    }
+
+
+    //select all icons under icons-wrap > ul#iconTray
+    const all = $("span.icons-wrap > ul#iconTray > li");
+
+    //hide all user lists
+    all.parent().parent().parent().parent().parent().hide();
+    all.css({"border": "none"}); //remove all borders
+
+    if (haveListvar != "li") {
+      $("span.icons-wrap.icons").find(haveListvar).css({"border": "2px solid red"}); //this is for putting a red border around icons
+    }
+    //find and then select all icons in have list and unhide/show the user list (since they were hidden)
+    $("span.icons-wrap.icons").find(haveListvar).parent().parent().parent().parent().parent().show();
+
     var i=0;
     for (; i<arrList.length; i++) {
 
-    //const remover = $("div.userlist-wrapper > ul.user-info-list-wrap.bottom-round > li >div.level-icons-wrap > span.user-icons > span.icons-wrap.icons > ul#iconTray > li"+arrList[i]+"");
-    // ^^^ Above is a longer version of vvv the one below.
+    const hider = $("ul#iconTray > li"+arrList[i]+""); //select all li child elements of ul#iconTray if they contain Id
 
-    const remover = $("ul#iconTray > li"+arrList[i]+""); //select all li child elements of ul#iconTray if they contain Id
+    //hide the parent>parent>parent>parent>parent element, which is the whole user info element
+    hider.parent().parent().parent().parent().parent().hide();
 
-    //removes/deletes the parent>parent>parent>parent>parent element, which is the whole user info element
-    remover.parent().parent().parent().parent().parent().remove();
 
-    //select all icons under icons-wrap > ul#iconTray, after the filtered out icons were deleted.
-    const all = $("span.icons-wrap > ul#iconTray > li");     //.css({"color": "blue", "border": "2px solid blue"}); //the .css part was for checking what icons were being selected, putting a blue border around them
-
-    //hide (not delete) all user lists
-    all.parent().parent().parent().parent().parent().hide();
-
-    //$("ul#iconTray").find("li#icon3,li#icon4").css({"color": "red", "border": "2px solid red"}); //this is for putting a red border around donator / subscriber icons
-
-    //find and then select all li#icon3 or li#icon4 under ul#iconTray, and unhide/show the user list (since they were hidden)
-    $("ul#iconTray").find("li#icon3,li#icon4").parent().parent().parent().parent().parent().show();
 
     } //end of for loop
+};//filter
 
-}); //end of mutationObserver
+const observer = new MutationObserver(() => {
+    filter();
+});
 
-//set observer to monitor changes in .content-wrapper div on page
-const wrapper = document.querySelector('.content-wrapper'); //select the element called .content-wrapper
-observer.observe(wrapper, { subtree: true, childList: true }); //observe changes in .content-wrapper
+//set observer to monitor changes in .userlist-wrapper div on page
+const wrapper = document.querySelector('.userlist-wrapper'); //select the element called .userlist-wrapper
+observer.observe(wrapper, { subtree: true, childList: true }); //observe changes in .userlist-wrapper
+
+$('input[type=checkbox]').change(function(){
+          filter();
+});
+
+filter();
+
+
+
+
+
+
+});//window.load
