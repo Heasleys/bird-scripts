1d84b4047ffaf6c355a7afa6ebf0a7454fb1929e
Heasley <heasleys4hemp@hotmail.com>
2020-01-30 08:21:01 -0500

    added clears and joins for both sides

--- total-joins.user.js
+++ total-joins.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Total Joins
 // @namespace    Heasleys.totaljoins
-// @version      0.3
+// @version      0.4
 // @description  Count total joins and clears for wall wars
 // @author       Heasleys4hemp [1468764]
 // @match        https://www.torn.com/war.php*
@@ -9,8 +9,9 @@
 // @updateURL    https://github.com/Heasleys/bird-scripts/raw/master/total-joins.user.js
 // ==/UserScript==
 
-    function countJoins() {
-        var total = 0;
+    function countFriendly() {
+        var totaljoins = 0;
+        var totalclears = 0;
         var num = 0;
 
         var joins = $('.your-faction').find('.joins');
@@ -18,32 +19,54 @@
         joins.each(function(){
             if ($.isNumeric(parseInt($(this).text()))) {
             num = parseInt($(this).text());
-            total += num;
+            totaljoins += num;
+        }
+        });
+
+        var clears = $('.your-faction').find('.knock-off');
+
+        clears.each(function(){
+            if ($.isNumeric(parseInt($(this).text()))) {
+            num = parseInt($(this).text());
+            totalclears += num;
         }
         });
 
         var title = $("span.your").text();
-        title += '- Joins: ' + total;
+        title += '- Joins: ' + totaljoins + ' - Clears: ' + totalclears;
         $("span.your").text(title);
     }
 
-    function countClears() {
-        var total = 0;
+
+
+
+    function countEnemy() {
+        var totaljoins = 0;
+        var totalclears = 0;
         var num = 0;
+
+        var joins = $('.enemy-faction').find('.joins');
+
+        joins.each(function(){
+            if ($.isNumeric(parseInt($(this).text()))) {
+            num = parseInt($(this).text());
+            totaljoins += num;
+        }
+        });
 
         var clears = $('.enemy-faction').find('.knock-off');
 
         clears.each(function(){
             if ($.isNumeric(parseInt($(this).text()))) {
             num = parseInt($(this).text());
-            total += num;
+            totalclears += num;
         }
         });
 
         var title = $("span.enemy").text();
-        title += ' - Clears: ' + total;
+        title += '- Joins: ' + totaljoins + ' - Clears: ' + totalclears;
         $("span.enemy").text(title);
     }
 
-    countJoins();
-    countClears();
+    countFriendly();
+    countEnemy();
