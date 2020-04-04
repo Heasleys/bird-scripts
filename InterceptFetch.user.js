7d0f35dc21d05e980c198abb8bcfcc00b4f08443
Heasley <heasleys4hemp@hotmail.com>
2020-04-04 19:16:40 -0400

    Update InterceptFetch.user.js

--- InterceptFetch.user.js
+++ InterceptFetch.user.js
@@ -1,54 +1,9 @@
 // ==UserScript==
 // @name         Intercept Fetch
-// @namespace    http://tampermonkey.net/
-// @version      0.1
+// @namespace    https://github.com/Heasleys/bird-scripts/raw/master/InterceptFetch.user.js
+// @version      1.0
 // @description  Different ways to intercept fetches/ajax requests on Torn
-// @author       Heasleys4hemp
-// @match        https://www.torn.com/*
-// @grant        none
-// ==/UserScript==
-
-
-(function () {
-    jQuery.ajaxSetup({
-        dataFilter: function (data, type) {
-            console.log(data);
-            return data;
-        }
-    });
-
-
-    $( document ).ajaxComplete(function( event, request, settings ) {
-        console.log(request.responseText);
-    });
-
-
-function interceptFetch(url, callback) {
-    var originalFetch = festch;
-    fetch = function() {
-        return originalFetch.apply(this, arguments).then(function(data) {
-            let dataurl = data.url.toString();
-            if (dataurl.indexOf(url)) {
-               const clone = data.clone();
-               clone.json().then((response) => callback(response, data.url));
-            }
-            return data;
-        });
-    };
-}
-
-    interceptFetch("torn.com", (response, url) => {
-     console.log("Found a fetch from: " + url);
-     console.log(response);
-    });
-
-})();
-// ==UserScript==
-// @name         Intercept Fetch
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  Different ways to intercept fetches/ajax requests on Torn
-// @author       Heasleys4hemp
+// @author       Heasleys4hemp [1468764]
 // @match        https://www.torn.com/*
 // @grant        none
 // ==/UserScript==
