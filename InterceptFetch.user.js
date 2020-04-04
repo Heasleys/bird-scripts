616830cd2b88afaaa576a9923b412884557a0b2f
Heasley <heasleys4hemp@hotmail.com>
2020-04-04 19:15:53 -0400

    Create InterceptFetch.user.js

---
+++ InterceptFetch.user.js
@@ -0,0 +1,90 @@
+// ==UserScript==
+// @name         Intercept Fetch
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Different ways to intercept fetches/ajax requests on Torn
+// @author       Heasleys4hemp
+// @match        https://www.torn.com/*
+// @grant        none
+// ==/UserScript==
+
+
+(function () {
+    jQuery.ajaxSetup({
+        dataFilter: function (data, type) {
+            console.log(data);
+            return data;
+        }
+    });
+
+
+    $( document ).ajaxComplete(function( event, request, settings ) {
+        console.log(request.responseText);
+    });
+
+
+function interceptFetch(url, callback) {
+    var originalFetch = festch;
+    fetch = function() {
+        return originalFetch.apply(this, arguments).then(function(data) {
+            let dataurl = data.url.toString();
+            if (dataurl.indexOf(url)) {
+               const clone = data.clone();
+               clone.json().then((response) => callback(response, data.url));
+            }
+            return data;
+        });
+    };
+}
+
+    interceptFetch("torn.com", (response, url) => {
+     console.log("Found a fetch from: " + url);
+     console.log(response);
+    });
+
+})();
+// ==UserScript==
+// @name         Intercept Fetch
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Different ways to intercept fetches/ajax requests on Torn
+// @author       Heasleys4hemp
+// @match        https://www.torn.com/*
+// @grant        none
+// ==/UserScript==
+
+
+(function () {
+    jQuery.ajaxSetup({
+        dataFilter: function (data, type) {
+            console.log(data);
+            return data;
+        }
+    });
+
+
+    $( document ).ajaxComplete(function( event, request, settings ) {
+        console.log(request.responseText);
+    });
+
+
+function interceptFetch(url, callback) {
+    var originalFetch = festch;
+    fetch = function() {
+        return originalFetch.apply(this, arguments).then(function(data) {
+            let dataurl = data.url.toString();
+            if (dataurl.indexOf(url)) {
+               const clone = data.clone();
+               clone.json().then((response) => callback(response, data.url));
+            }
+            return data;
+        });
+    };
+}
+
+    interceptFetch("torn.com", (response, url) => {
+     console.log("Found a fetch from: " + url);
+     console.log(response);
+    });
+
+})();
