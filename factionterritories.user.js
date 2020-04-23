68431a911d2740f2682316f3839ae0b33a9efa4a
Heasley <heasleys4hemp@hotmail.com>
2020-04-23 14:03:18 -0400

    Update factionterritories.user.js

--- factionterritories.user.js
+++ factionterritories.user.js
@@ -11,6 +11,7 @@
 // ==/UserScript==
 
 var APIKEY = localStorage.getItem('wb_apikey') || '';
+var factionlist;
 
 GM_addStyle(`
 .wb_container {
@@ -18,22 +19,29 @@ GM_addStyle(`
   flex-direction: column;
 }
 
-.wb_title {
-  border: 1px solid rgba(0, 0, 0, .5);
+div.wb_head {
+  border-bottom: none;
+  border-radius: 5px 5px 5px 5px;
+  box-shadow: rgba(0, 0, 0, 0.25) 0px 1px 3px;
+  padding: 6px 10px;
+  background-color: rgb(202, 185, 0);
+  background-image: linear-gradient(90deg, transparent 50%, rgba(0, 0, 0, 0.07) 0px);
+  background-size: 4px;
+  cursor: pointer;
+}
+
+div.wb_head.expanded {
   border-bottom: none;
   border-radius: 5px 5px 0px 0px;
+}
+
+span.wb_title {
   color: #ffffff;
   font-size: 13px;
   letter-spacing: 1px;
   text-shadow: rgba(0, 0, 0, 0.65) 1px 1px 2px;
-  font-weight: 400;
-  padding-left: 10px;
-  margin-top: 10px;
-  background-color: rgb(202, 185, 0);
-  background-image: linear-gradient(90deg, transparent 50%, rgba(0, 0, 0, 0.07) 0px);
-  background-size: 4px;
-  height: 30px;
-  line-height: 30px;
+  font-weight: 700;
+  line-height: 16px;
 }
 
 span.wb_error {
@@ -51,6 +59,7 @@ span.wb_success {
 .wb_content {
   background-color: #F2F2F2;
   border: 1px solid rgba(0, 0, 0, .5);
+  border-radius: 0px 0px 5px 5px;
   border-top: none;
 }
 
@@ -93,6 +102,22 @@ span.wb_success {
   cursor: pointer;
 }
 
+span.wb_icon {
+  align-items: center;
+  justify-content: center;
+  width: 16px;
+  float: right;
+}
+
+span.wb_icon svg {
+  display: block;
+  height: 16px;
+  fill: white;
+  cursor: pointer;
+  margin-left: auto;
+  margin-right: auto;
+}
+
 .wb_input {
   width: 118px;
   height: 23px;
@@ -131,49 +156,25 @@ span.wb_success {
 
 $(window).load(function(){
 
-    $('#wb_apikey_input').val(APIKEY);
-
-    $(".wb_hide").click(function() {
-        $("#api_input").slideToggle("slow");
-    });
-
 $( document ).ajaxComplete(function(event, jqXHR, ajaxObj) {
+
   if (ajaxObj.url && ajaxObj.url.includes('step=mapData')) {
+      createWBHeader();
       const mapdata = JSON.parse(jqXHR.responseText);
 
-      var factionlist = mapdata.factionOwnTerritories.factionData;
+      factionlist = mapdata.factionOwnTerritories.factionData;
 
-      getAPI(APIKEY,factionlist);
-
+      $('#wb_apikey_input').val(APIKEY);
+      getAPI(APIKEY);
   }
-
-
-    $('#flistinput').change(function(){
-      var id = $(this).val();
-
-        if (id != "") {
-             var name = $('#flist [value="' + id + '"]').data('name');
-             var terr = $('#flist [value="' + id + '"]').data('terr');
-
-             window.location = "#terrName=" + terr;
-        }
-
-    });
-
-    $('#wb_save_apikey').click(function() {
-      APIKEY = $('#wb_apikey_input').val();
-      localStorage.setItem('wb_apikey', APIKEY);
-        getAPI(APIKEY,factionlist);
-    });
-
-
 
 });
 
 });//window.load
 
 
-function getAPI(APIKEY,flist) {
+function getAPI(APIKEY) {
+    let flist = factionlist;
     if (APIKEY != "") {
 
     var request_url = 'https://api.torn.com/torn/?selections=territory&key='+APIKEY;
@@ -216,12 +217,12 @@ function getAPI(APIKEY,flist) {
     }//if empty
 }//end function
 
-createWBHeader() {
-  
+function createWBHeader() {
+
   document.querySelector('#tab-menu').insertAdjacentHTML('beforebegin', `
     <div class="wb_container">
-      <div class="wb_title">Faction Territories<span class="wb_error" hidden></span><span class="wb_success" hidden> - Factions loaded</span></div>
-       <div class="wb_content">
+      <div class="wb_head"><span class="wb_title">Faction Territories</span><span class="wb_error" hidden></span><span class="wb_success" hidden> - Factions loaded</span><span class="wb_toggle wb_icon" id="wb_svg_right"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 32"><path d="M4 6l-4 4 6 6-6 6 4 4 10-10L4 6z"></path></svg></span><span class="wb_toggle wb_icon" id="wb_svg_down" hidden><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 32"><path d="M16 10l-6 6-6-6-4 4 10 10 10-10-4-4z"></path></svg></span></div>
+       <div class="wb_content" hidden>
         <div class="wb_row">
           <div class="wb_col">
             <p>Search List</p>
@@ -236,7 +237,7 @@ createWBHeader() {
 
           </div>
           <div class="wb_col">
-          <p class="wb_hide">API Key ▼</p>
+          <p class="wb_hide" data-target="#api_input">API Key ▼</p>
            <div id="api_input" hidden>
              <input class="wb_input wb_input_group" type="text" id="wb_apikey_input" required minlength="16" maxlength="16">
              <button class="wb_input_button wb_input_group" type="button" id="wb_save_apikey">Save</button>
@@ -249,5 +250,37 @@ createWBHeader() {
     </div>
     <hr class="delimiter-999 m-top10">
     `);
-  
+
+    $(".wb_hide").click(function() {
+        let target = $(this).data('target');
+        $(target).slideToggle("slow");
+    });
+
+    $(".wb_head").click(function() {
+      $(this).toggleClass("expanded");
+      $(".wb_content").slideToggle("slow");
+
+      if (!$("#wb_svg_right").is(':visible')) {
+          $("#wb_svg_right").attr("hidden",false);
+          $("#wb_svg_down").attr("hidden",true);
+      } else {
+          $("#wb_svg_right").attr("hidden",true);
+          $("#wb_svg_down").attr("hidden",false);
+      }
+    });
+
+     $('#wb_save_apikey').click(function(event) {
+        APIKEY = $('#wb_apikey_input').val();
+        localStorage.setItem('wb_apikey', APIKEY);
+        getAPI(APIKEY);
+     });
+
+     $('#flistinput').change(function(){
+      var id = $(this).val();
+        if (id != "") {
+          var name = $('#flist [value="' + id + '"]').data('name');
+          var terr = $('#flist [value="' + id + '"]').data('terr');
+          window.location = "#terrName=" + terr;
+        }
+     });
 }
