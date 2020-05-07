2056c6999bb3a6a7016c4728393ab4048d67c9e9
Heasley <heasleys4hemp@hotmail.com>
2020-05-06 22:54:25 -0400

    Create portfolio-tweaks.user.js

---
+++ portfolio-tweaks.user.js
@@ -0,0 +1,43 @@
+// ==UserScript==
+// @name         Portfolio Tweaks
+// @namespace    Heasleys.portfoliotweaks
+// @version      0.1
+// @description  Adds net change to stocks
+// @author       Heasleys4hemp [1468764]
+// @match        https://www.torn.com/stockexchange.php?step=portfolio
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    $('ul.stock-cont > li.item-wrap').each(function() {
+        let dir = '';
+        let dirclass = '';
+        let acro = $(this).data('stock');
+        let info_tab = $(this).find('ul > li.info');
+
+        let shares_tab = info_tab.find('div.b-price-wrap > div:contains("Shares:")');
+        let shares = parseInt(shares_tab.text().replace(/\D/g,''));
+
+        let length_wrap = info_tab.find('div.length-wrap');
+
+        if (length_wrap.find('span.change.down').length) {dir = '-';}else{dir = '+'}
+
+        let change_tab = length_wrap.find('span.value');
+        let change = change_tab.text().split(" ").shift().replace('$','');
+
+        let net = Math.floor(change * shares);
+
+        let new_tab = length_wrap.find('span.action');
+        if (dir == '-') {dirclass = 'bold change down';}
+        if (dir == '+') {dirclass = 'bold change up';}
+
+        let netFormatted = net.toLocaleString('en-US');
+
+        new_tab.append('<span class="bold">Net Change: </span>');
+        new_tab.append('<span class="'+dirclass+'">' + dir + '$' + netFormatted + '</span>');
+
+
+    });
+})();
