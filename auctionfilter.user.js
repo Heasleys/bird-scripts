62d05053e22f68a81758bbb3ee1452a8cd63fb3b
Heasley <heasleys4hemp@hotmail.com>
2020-11-12 16:45:16 -0500

    fixed blank search
    Blank search now reshows all hidden items

--- auctionfilter.user.js
+++ auctionfilter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Auction Filter
 // @namespace    Heasleys.AuctionFilter
-// @version      1.1
+// @version      1.1.1
 // @description  Filter Items from the Torn Auction
 // @author       Heasleys4hemp [1468764]
 // @match        https://www.torn.com/amarket.php*
@@ -71,7 +71,7 @@ GM_addStyle(`
 `);
 
 $(window).load(function () {
-document.querySelector('#auction-house-tabs').insertAdjacentHTML('beforebegin', `
+    document.querySelector('#auction-house-tabs').insertAdjacentHTML('beforebegin', `
     <div class="wb_container">
       <div class="wb_title">Search Market</div>
        <div class="wb_content">
@@ -87,59 +87,65 @@ document.querySelector('#auction-house-tabs').insertAdjacentHTML('beforebegin', 
     <hr class="delimiter-999 m-top10">
     `);
 
-//copy pagination-wrap (the page selector element) and paste it to the top of it's own parent element
-$('.pagination-wrap').each(function(i, obj) {
-    $(this).clone().prependTo($(this).parent());
-});
+    //copy pagination-wrap (the page selector element) and paste it to the top of it's own parent element
+    $('.pagination-wrap').each(function(i, obj) {
+        $(this).clone().prependTo($(this).parent());
+    });
 
-function filter() {
-//array list of items to hide
-//I plan on making this better in the future, but it works for what I need it for.
-var arrList = ['Guandao','Salt Shaker','Cricket Bat', 'Psycho Clown Mask', 'Twin Tiger Hooks', 'Old Lady Mask', 'Scarred Man Mask', 'Nun Mask', 'Young Lady Mask', 'Ginger Kid Mask', 'Moustache Man Mask', 'Kama', 'Exotic Gentleman Mask', 'Wushu Double Axes', 'Mini Cooper S'];
-//MutationObserver used to monitor specific activity
+    function filter() {
+        //array list of items to hide
+        //I plan on making this better in the future, but it works for what I need it for.
+        var arrList = ['Guandao','Salt Shaker','Cricket Bat', 'Psycho Clown Mask', 'Twin Tiger Hooks', 'Old Lady Mask', 'Scarred Man Mask', 'Nun Mask', 'Young Lady Mask', 'Ginger Kid Mask', 'Moustache Man Mask', 'Kama', 'Exotic Gentleman Mask', 'Wushu Double Axes', 'Mini Cooper S'];
+        //MutationObserver used to monitor specific activity
 
-    //observing item list div for li's that contain 'Cricket Bat' (now list of items)
-    var i=0;
-    for (; i<arrList.length; i++) {
-        const cricket = $("div.items-list-wrap > ul.items-list.t-blue-cont.h > li:contains('"+arrList[i]+"')");
-        cricket.hide();
-    }
+        //observing item list div for li's that contain 'Cricket Bat' (now list of items)
+        var i=0;
+        for (; i<arrList.length; i++) {
+            const cricket = $("div.items-list-wrap > ul.items-list.t-blue-cont.h > li:contains('"+arrList[i]+"')");
+            cricket.hide();
+        }
 
-}//end of filter
+    }//end of filter
 
-$( "#afind" ).change(search());
+    $( "#afind" ).change(search());
 
     function search() {
-        if ($('#afind').val()) {
-            var itemname = $("span.title > span.item-name");
+        //if ($('#afind').val()) {
+        var itemname = $("span.title > span.item-name");
 
-            itemname.each(function( index ) {
-                $(this).parent().parent().parent().hide();
+        itemname.each(function( index ) {
+            $(this).parent().parent().parent().hide();
 
-                var s1 = $( this ).text().toLowerCase();
-                var s2 = $('#afind').val().toLowerCase();
+            var s1 = $( this ).text().toLowerCase();//item name
+            var s2 = $('#afind').val().toLowerCase();//search term
 
+            if (s2) {
                 if(s1.indexOf(s2) !== -1) {
                     $(this).parent().parent().parent().show();
                 }
-            });
-        }
+            } else {
+                $(this).parent().parent().parent().show();
+            }
+
+
+        });
+        //}
     }
 
-//set observer to monitor changes in .content-wrapper div on page
-const target = document.querySelector('.content-wrapper');
-  create(target);
+    //set observer to monitor changes in .content-wrapper div on page
+    const target = document.querySelector('.content-wrapper');
+    create(target);
 
 
-function create(t) {
-  // create an observer instance
-  var observer = new MutationObserver(function(mutations) {
+    function create(t) {
+        // create an observer instance
+        var observer = new MutationObserver(function(mutations) {
 
-      filter();
-      search();
+            filter();
+            search();
 
-  });
-observer.observe(t, { subtree: true, childList: true });
-}
+        });
+        observer.observe(t, { subtree: true, childList: true });
+    }
 
 });
