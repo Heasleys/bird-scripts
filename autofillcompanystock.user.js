9ddbad90f65db7d2fedeeee0059f5ea9b38015dc
Heasley <heasleys4hemp@hotmail.com>
2021-08-29 18:53:51 -0400

    Update autofillcompanystock.user.js

--- autofillcompanystock.user.js
+++ autofillcompanystock.user.js
@@ -1,29 +1,39 @@
 // ==UserScript==
-// @name         Autofill Company Stock
-// @namespace    Heasleys.autofillcompanystock
-// @version      1.0
-// @description  Fill company stock from last sold.
+// @name         Company Tweaks
+// @namespace    Heasleys.companytweaks
+// @version      1.1
+// @description  Fill company stock from last sold, highlight inactive users and users with -20+ addiction
 // @author       Heasleys4hemp [1468764]
 // @match        https://www.torn.com/companies.php
 // @icon         https://www.google.com/s2/favicons?domain=torn.com
 // @grant        none
 // ==/UserScript==
 
+if (location.hash === "#/option=stock") {
+    setTimeout(fillStock, 1000);
+}
+
+if (location.hash === "#/option=employees") {
+    setTimeout(checkInactive, 1000);
+}
+
+
 window.onhashchange = function () {
-    console.log(location.hash);
     if (location.hash === "#/option=stock") {
         setTimeout(fillStock, 1000);
     }
+
+    if (location.hash === "#/option=employees") {
+        setTimeout(checkInactive, 1000);
+    }
 };
 
-if (location.hash === "#/option=stock") {
-    setTimeout(fillStock, 1000);
-    }
+
 
 
 
 function fillStock() {
-console.log($('ul.stock-list > li'));
+
     $('ul.stock-list > li').each(function() {
         let stockSold = $(this).find('div.sold-daily').text();
 
@@ -35,6 +45,26 @@ console.log($('ul.stock-list > li'));
 
         input.val(num);
 
+    });
+
+}
+
+function checkInactive() {
+    $('ul.employee-list > li').each(function() {
+        let eff = $(this).find('div.effectiveness');
+        if (eff.length != 0) {
+            let aria = eff.attr('aria-label');
+            console.log(aria);
+            if (aria) {
+                if (aria.includes('Inactivity:')) {
+                    eff.css('background-color', 'rgba(255, 25, 0, 0.75)');
+                }
+
+                if (/(Addiction: -2)\d/.test(aria)) {
+                    eff.css('background-color', 'rgba(255, 25, 0, 0.25)');
+                }
+            }
+        }
     });
 
 }
